d3fb238fb57625db220c6c487f4158741c169155
Oleg Podsechin <oleg.podsechin@ionsquare.com>
2012-03-10 20:01:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('http').createServer(function(req, res) {
 	res.writeHead(200, {
 		'Content-Type': 'text/plain'
 	});
-	res.end(process.cwd() + '\n');
+	res.end('cwd: ' + process.cwd() + '\n');
 }).listen(process.env.HTTP_PORT || 8080);
 
 
